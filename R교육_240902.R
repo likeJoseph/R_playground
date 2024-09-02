9f6bab3ec2356c9ab8ab64127b16087d654a2918
igchong <igchong@gmail.com>
2024-09-02 12:04:15 +0900

    no message

--- R교육_240902.R
+++ R교육_240902.R
@@ -1,6 +1,13 @@
 setwd('c:/data/exam_r')
 
+library(data.table)
 library(dplyr)
 a <- 1
 b <- "Hello"
 c <- c(1, 2, 3, 4, 5)
+
+data <- read.csv("data handling_dplyr.csv", fileEncoding = 'euc-kr')
+
+sum(is.na(data))
+colSums(is.na(data))
+na.omit(data)
