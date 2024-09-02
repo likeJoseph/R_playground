d5e96df55c196eff236c55313a7c7cc0af67f0bb
igchong <igchong@gmail.com>
2024-09-02 13:29:21 +0900

    수정

--- R교육_240902.R
+++ R교육_240902.R
@@ -10,4 +10,15 @@ data <- read.csv("data handling_dplyr.csv", fileEncoding = 'euc-kr')
 
 sum(is.na(data))
 colSums(is.na(data))
-na.omit(data)
+exam_na <- na.omit(data)
+
+# 파생변수 생성하기
+# mutate() 함수 사용
+# 수식이 있는 경우
+# 근태점수 생성하기
+# 데이터: exam_na(결측치가 제거된 데이터)
+
+a5_1 <- mutate(exam_na, 근태점수 = 100 - (결근횟수 + (지각횟수/3))*5)
+a5_1
+
+a5_1[1:3,]
